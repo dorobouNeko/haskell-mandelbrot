aab1b29a7c01e0ca598bb6923d65b3ac65759d49
dorobouNeko <gonzalob1302@protonmail.com>
2018-10-19 23:42:36 -0300

    clean main program and use case for args

--- main-sdl2.hs
+++ main-sdl2.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
---module Main where
 
 import qualified SDL
 import Control.Monad (unless)
@@ -8,44 +7,58 @@ import System.Environment
 import Data.Function (on)
 import Foreign.C.Types (CFloat,CInt)
 
+
 main :: IO ()
 main = do
 	args <- getArgs -- maxW maxH lB rB uB dB green=255 blue=255
+	case args of
+		[mw, mh, lB, rB, uB, dB] -> do
+			let
+				(maxRes, c) = splitAt 2 args
+				(b,color) = splitAt 4 c
+				([mw, mh],[lB, rB, uB, dB]) =
+				 	(map strToInt maxRes
+				 	, map strToDouble b)
+			runProgram mw mh lB rB uB dB 255 255
+		_ -> do print "failure"
+
+runProgram mw mh lB rB uB dB green blue = do
 	let
-		(maxRes, c) = splitAt 2 args
-		(b,color) = splitAt 4 c
- 		([mw, mh],[lB, rB, uB, dB]) =
- 			if null maxRes
- 				then ([640,480], map strToDouble b)
- 				else (map strToInt maxRes, map strToDouble b)
- 		[green,blue] = if null color then [255,255] else (map strToInt color)
- 		bounds = (lB,rB,uB,dB)
+		bounds = (lB,rB,uB,dB)
  		(w,h) = (rB - lB, uB - dB)
 		(px,py) = M.pixelSize (mw,mh) w h
 		windowSize = v2CInt (px, py)
 		(s0,py') = M.points (px,py) bounds
-		offset = if uB > -dB then py-py' else -(py-py')
-		hue = cycle $ filter (\x -> mod x 23 == 0) ([0..255]++(reverse [1..254]))
+		offset =
+			if uB > -dB
+			then   py-py'
+				else -(py-py')
+		hue =
+			cycle $
+				filter (\x -> mod x 23 == 0) ([0..255]++(reverse [1..254]))
 	SDL.initializeAll
 {- debug option
-	print (px,py,"dimensions")
-	print (py',"point height")
-	print (offset,"offset")
-	print (length s0)
-	print (head s0)
-	print (last s0)
+			print (px,py,"dimensions")
+			print (py',"point height")
+			print (offset,"offset")
+			print (length s0)
+			print (head s0)
+			print (last s0)
 -}
 	window <- SDL.createWindow "SDL Application" SDL.defaultWindow
-		{ SDL.windowInitialSize  = windowSize }
---		, SDL.windowOpenGL = Just SDL.defaultOpenGL }
-
---	glContext <- SDL.glCreateContext window
---	SDL.swapInterval SDL.$= SDL.ImmediateUpdates
-
+			{ SDL.windowInitialSize  = windowSize }{-
+			, SDL.windowOpenGL = Just SDL.defaultOpenGL }
+			-}
+{-
+		glContext <- SDL.glCreateContext window
+		SDL.swapInterval SDL.$= SDL.ImmediateUpdates
+-}
 	renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
---	SDL.rendererScale renderer SDL.$= (v2CFloat (s, s))
-
+{-
+		SDL.rendererScale renderer SDL.$= (v2CFloat (s, s))
+-}
 	appLoop renderer s0 [] 0 hue offset py green blue
+
 
 v2CFloat :: (Int,Int) -> (SDL.V2 CFloat)
 v2CFloat      (x, y)   = (SDL.V2 `on` fromIntegral) x y
