4c2fa7e3d67ec7518d5871ca1504f3b18acd4c00
dorobouNeko <gonzalob1302@protonmail.com>
2018-10-21 14:28:57 -0300

    pretty up help text

--- Argparser.hs
+++ Argparser.hs
@@ -3,7 +3,7 @@ module Argparser where
 import System.Exit
 import Data.List.Split (splitOn)
 
---  [-s horizontal vertical] [-b left right top bottom] [-c greenHue blueHue]
+-- [-h | --help] [[-s horizontal vertical] [-b left right top bottom] [-c greenHue blueHue]]
 argParser :: [String] -> IO [String]
 argParser args =
 	case listOfArgList of
@@ -51,12 +51,19 @@ argParser args =
 					else ["noDashes"]
 		listOfArgList = map words splitArgs
 
-
+help :: IO b
 help =
+	putStrLn "Usage:" >>
+	putStrLn "" >>
+	putStrLn "Main -h" >>
+	putStrLn "Main --help" >>
 	putStrLn "Main [-s horizontal vertical] [-b left right top bottom] [-c greenHue blueHue]" >>
-	putStrLn "-s : maximum horizontal and vertical size in pixels" >>
-	putStrLn "-b : bounding coordinates for drawing the Mandelbrot set" >>
-	putStrLn "-c : set color hues for green and blue (0 to 255)" >>
+	putStrLn "" >>
+	putStrLn "-h, --h : This help text :)" >>
+	putStrLn "-s      : Maximum horizontal and vertical size in pixels of display window" >>
+	putStrLn "-b      : Bounding coordinates for drawing the Mandelbrot set" >>
+	putStrLn "-c      : Set color hues for green and blue (0 to 255)" >>
 	exitSuccess
 
+failedToParse :: IO a
 failedToParse = die "Failed to parse arguments. Use -h or --help for more information."
